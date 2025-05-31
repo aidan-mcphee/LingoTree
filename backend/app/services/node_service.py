362eb58972da1a841a4b5649893b62f905da3ad3
Aidan Mcphee <aidanmcpheecompsci@gmail.com>
2025-05-31 18:41:22 +0900

    removed backend folder

--- backend/app/services/node_service.py
+++
@@ -1,6 +0,0 @@
-from flask import current_app
-
-def get_Nodes():
-    supabase = current_app.supabase
-    response = supabase.table('nodes').select('*').execute()
-    return response.data
--- backend/wsgi.py
+++
@@ -1,7 +0,0 @@
-from app import create_app
-
-# Create the Flask application using the factory pattern
-app = create_app()
-
-# Expose the app as 'application' for WSGI compatibility
-application = app
